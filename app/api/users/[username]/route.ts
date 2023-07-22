8c74a6dcd6cba57a42059a7acb747037cb98d5ea
standardwish <standardstar@hanyang.ac.kr>
2023-07-22 22:00:57 +0900

    fix lint error

--- app/api/users/[username]/route.ts
+++ app/api/users/[username]/route.ts
@@ -1,9 +1,9 @@
 import connectMongoDB from "@/lib/mongodb";
 import users from "@/models/users";
 import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextApiRequest, { params }: any) {
+export async function GET(request: NextRequest, { params }: any) {
   const { username } = params;
   await connectMongoDB();
 
