21e009b414a651c0751bf340d1d44e225927d6ce
standardwish <standardstar@hanyang.ac.kr>
2023-07-23 23:03:35 +0900

    feat: add nextauth secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -52,7 +52,7 @@ const options: NextAuthOptions = {
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(options);
