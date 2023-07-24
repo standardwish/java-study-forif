8d2c3916e1900ace0420667f864637ef28ea160f
standardwish <standardstar@hanyang.ac.kr>
2023-07-24 16:30:22 +0900

    remove middleware

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export { default } from "next-auth/middleware";
-export const config = {
-  matcher: ["/api"],
-  runtime: "experimental-edge",
-};
