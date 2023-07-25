aa4d0d643b57f03df3109342c7d2f85c0b8ef9e4
standardwish <standardstar@hanyang.ac.kr>
2023-07-25 12:10:50 +0900

    header pt-20 add

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,17 @@ import Loading from "./loading";
 
 const UnAuthPage = () => {
   return (
-    <div className="text-8xl md:text-4xl leading-normal text-center items-center flex justify-center font-bold h-screen mt-[-10rem]">
-      <h1 className={UbunTu.className}>
-        {useTypingWords("It's Time", 100)}
-        <br />
-        {useTypingWords("To", 120)}
-        <br />
-        {useTypingWords("Learn ", 140)}
-        <span className="text-red-500">{useTypingWords("Java", 160)}</span>
-      </h1>
+    <div className={UbunTu.className}>
+      <div className="text-8xl md:text-4xl leading-normal text-center items-center flex justify-center font-bold h-screen pt-20">
+        <h1 className="relative mt-[-15rem]">
+          {useTypingWords("It's Time", 100)}
+          <br />
+          {useTypingWords("To", 120)}
+          <br />
+          {useTypingWords("Learn ", 140)}
+          <span className="text-red-500">{useTypingWords("Java", 160)}</span>
+        </h1>
+      </div>
     </div>
   );
 };
@@ -26,8 +28,8 @@ const AuthPage = () => {
   const { data: session } = useSession();
   return (
     <div className={UbunTu.className}>
-      <div className="relative text-8xl md:text-4xl leading-normal items-center flex flex-col justify-center font-bold h-screen mt-[-10rem]">
-        <div className="relative animate-fadein">
+      <div className="relative text-8xl md:text-4xl leading-normal items-center flex flex-col justify-center font-bold h-screen pt-20">
+        <div className="relative animate-fadein mt-[-15rem]">
           <h1>포리프 자바 스터디</h1>
           <Link href="/">
             <Notification />
@@ -51,7 +53,7 @@ function MainPage() {
 
   return (
     <Suspense fallback={<Loading />}>
-      <div className="bg-black bg-fixed w-full scroll-smooth pt-20">
+      <div className="bg-black bg-fixed w-full scroll-smooth">
         <div className=" text-white mx-auto w-2/3">
           {status === "loading" ? (
             <Loading />
