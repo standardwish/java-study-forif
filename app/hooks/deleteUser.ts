5eddd42bb8aec6f9d5a7fa9562bfc7a760fca910
standardwish <standardstar@hanyang.ac.kr>
2023-07-28 12:57:42 +0900

    add ENVIRONMENT

--- app/hooks/deleteUser.ts
+++ app/hooks/deleteUser.ts
@@ -1,5 +1,4 @@
 import type { IDeleteUser } from "@/types/type";
-
 /**
  * 유저 삭제
  * @param 삭제할 유저의 이름, 학과, 어드민 비밀번호
@@ -10,7 +9,9 @@ export const deleteUser = async ({ uid, adminPassword }: IDeleteUser) => {
     process.env.NODE_ENV === "development"
       ? `http://localhost:3000/api/users?id=${uid}`
       : `https://java-study-forif.vercel.app/api/users?id=${uid}`;
-  if (adminPassword === "1234") {
+  console.log(process.env.ADMIN_PASSWORD);
+
+  if (adminPassword === process.env.ADMIN_PASSWORD) {
     try {
       const res = await fetch(URI, {
         headers: { "Content-Type": "application/json" },
