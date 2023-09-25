4b7c8396f4522f948960e8557c309e71e1bbd51f
standardwish <standardstar@hanyang.ac.kr>
2023-09-25 13:11:24 +0900

    add week 2

--- app/assignment/page.tsx
+++ app/assignment/page.tsx
@@ -37,6 +37,33 @@ export default function Page() {
               조건문과 반복문2 - 쉬움
             </a>
           </div>
+          <h2 className="text-lg">2주차</h2>
+          <div className="flex flex-row gap-10">
+            <a
+              href="https://galmaehs.notion.site/2b384355383b4f44b89adcee5f73bb67?pvs=4"
+              target="_blank"
+            >
+              조건문과 반복문3(선택) - 중간
+            </a>
+            <a
+              href="https://galmaehs.notion.site/String-4c8a8d7c06584bb0ac8743946a893978?pvs=4"
+              target="_blank"
+            >
+              String과 다차원 배열1 - 쉬움
+            </a>
+            <a
+              href="https://galmaehs.notion.site/String-59c5fcb0b58043529703e13e365a0f00?pvs=4"
+              target="_blank"
+            >
+              String과 다차원 배열2 - 쉬움
+            </a>
+            <a
+              href="https://galmaehs.notion.site/String-607a47a0207f421cb0633aee04f81dbe?pvs=4"
+              target="_blank"
+            >
+              String과 다차원 배열3 - 중간
+            </a>
+          </div>
         </div>
       </div>
     </div>
